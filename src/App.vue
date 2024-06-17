44d3b6b3b49eda07613c4721ba03787a31c60dea
SaltyUA <104589666+SaltyUA@users.noreply.github.com>
2024-06-17 17:14:44 +0300

    first lesson

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import iButton from './components/iButton/iButton.vue';
+</script>
+
+<template>
+  <main class="flex h-screen">
+    <section class="flex-1 flex justify-center items-center px-5 bg-primary">
+      <div class="text-white text-center">
+        <img class="inline mb-6" src="./assets/img/map-pin.svg" alt="">
+        <h1 class="font-bold text-4xl mb-7">IT Traveler</h1>
+        <p class="leading-6 mb-11">Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а
+          також
+          ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.</p>
+
+        <iButton></iButton>
+      </div>
+    </section>
+    <section>
+      <img class="h-full object-cover" src="./assets/img/static-map.png" alt="">
+    </section>
+  </main>
+  Hello Vue<iButton></iButton>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F3743D'
+      }
+    }
+  },
+  plugins: []
+}
